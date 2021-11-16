01b0f5d76afff7b46f633ce577161d71d8973b1a
varun <varunlad5@gmail.com>
2021-11-16 23:29:58 +0530

    [Varun]Add:Created RegisterModel class

---
+++ FundooModel/RegisterModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace FundooModel
+{
+    class RegisterModel
+    {
+        public class RegistrationModel
+        {
+            [Required]
+            public string FirstName { get; set; }
+            [Required]
+            public string LastName { get; set; }
+            [Required]
+            public string Email { get; set; }
+            [Required]
+            public string Password { get; set; }
+        }
+    }
+}
