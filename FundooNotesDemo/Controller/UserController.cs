956556369c46beda89f56ac2c6395dda6c8c1351
varun <varunlad5@gmail.com>
2021-11-16 23:54:07 +0530

    [Varun]Add: UserController File is added in Controller Folder

---
+++ FundooNotesDemo/Controller/UserController.cs
@@ -0,0 +1,42 @@
+﻿using FundooManager.Manager;
+using FundooModel;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FundooNotesDemo.Controller
+{
+    public class UserController
+    {
+        private readonly IUserManager manager;
+
+        public UserController(IUserManager manager)
+        {
+            this.manager = manager;
+        }
+        [HttpPost]
+        [Route("api/register")]
+        public IActionResult Register([FromBody] RegisterModel userData)
+        {
+            try
+            {
+                string result = this.manager.Register(userData);
+
+                if (result.Equals("Registration Successfull"))
+                {
+                    return this.Ok(new ResponseModel<string>() { Status = true, Message = result });
+                }
+                else
+                {
+                    return this.BadRequest(new ResponseModel<string>() { Status = false, Message = result });
+                }
+            }
+            catch (Exception ex)
+            {
+                return this.NotFound(new ResponseModel<string>() { Status = false, Message = ex.Message });
+            }
+        }
+    }
+}
