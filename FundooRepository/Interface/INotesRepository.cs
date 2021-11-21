c339de1b70e114eadb2db88f9296318a68b9a972
varun <varunlad5@gmail.com>
2021-11-21 22:49:57 +0530

    [Varun]Add:NoteRepository class and its Interface is created

---
+++ FundooRepository/Interface/INotesRepository.cs
@@ -0,0 +1,12 @@
+﻿using FundooModel;
+using Microsoft.Extensions.Configuration;
+
+namespace FundooRepository.Repository
+{
+    public interface INotesRepository
+    {
+        IConfiguration Configuration { get; }
+
+        string Notes(NotesModel notes);
+    }
+}
