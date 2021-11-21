7e6bafa92473e5cd7f3652034df949eadf605864
varun <varunlad5@gmail.com>
2021-11-22 00:11:25 +0530

    [Varun]Add:Notes Manager is added

---
+++ FundooManager/Interface/INotesManager.cs
@@ -0,0 +1,9 @@
+﻿using FundooModel;
+
+namespace FundooManager.Interface
+{
+    public interface INotesManager
+    {
+        string Notes(NotesModel notes);
+    }
+}
---
+++ FundooManager/Manager/NotesManager.cs
@@ -0,0 +1,28 @@
+﻿using FundooModel;
+using FundooRepository.Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FundooManager.Manager
+{
+    public class NotesManager : INotesManager
+    {
+        private readonly INotesRepository repository;
+        public NotesManager(INotesRepository repository)
+        {
+            this.repository = repository;
+        }
+        public string Notes(NotesModel notes)
+        {
+            try
+            {
+                return this.repository.Notes(notes);
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+    }
+}
