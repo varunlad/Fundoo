62956675445ed1d610aa42aabc65b60ebf85781b
varun <varunlad5@gmail.com>
2021-11-21 23:17:49 +0530

    [Varun]Add:Controller class is created

--- FundooModel/ResetPasswordModel.cs
+++ FundooModel/ResetPasswordModel.cs
@@ -10,6 +10,6 @@ namespace FundooModel
         [Required]
         public string Email { get; set; }
         [Required]
-        public string NewPassword { get; set; }
+        public string NewPassword { get; set ; }
     }
 }
