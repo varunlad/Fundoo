af7993e81cadd99615d7934a98b59ab636746195
varun <varunlad5@gmail.com>
2021-11-27 15:30:13 +0530

    [Varun]Add:Manager class is created for Label

---
+++ FundooManager/Interface/ILableManager.cs
@@ -0,0 +1,10 @@
+﻿using FundooModel;
+using System.Threading.Tasks;
+
+namespace FundooManager.Interface
+{
+    public interface ILableManager
+    {
+        Task<string> Lable(LableModel lable);
+    }
+}
---
+++ FundooManager/Manager/LableManager.cs
@@ -0,0 +1,30 @@
+﻿using FundooManager.Interface;
+using FundooModel;
+using FundooRepository.Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FundooManager.Manager
+{
+    public class LableManager : ILableManager
+    {
+        private readonly ILableRepository repository;
+        public LableManager(ILableRepository repository)
+        {
+            this.repository = repository;
+        }
+        public async Task<string> Lable(LableModel lable)
+        {
+            try
+            {
+                return await this.repository.Lable(lable);
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+    }
+}
