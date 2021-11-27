3f4165d4aff3fe4e93104d93d0554c25b126e9eb
varun <varunlad5@gmail.com>
2021-11-27 15:20:45 +0530

    [Varun]Add:Model class is created for Label

---
+++ FundooModel/lableModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace FundooModel
+{
+    public class LableModel
+    {
+        [Key]
+        public int LableId { get; set; }
+        public string Lable { get; set; }
+
+        // Foreign key to LabeId
+        [ForeignKey("NotesModel")]
+        public int? NoteID { get; set; }
+        public virtual NotesModel NotesModel { get; set; }
+        // Foreign key to LabeId
+        [ForeignKey("RegisterModel")]
+        public int UserID { get; set; }
+        public virtual RegisterModel RegistrationModel { get; set; }
+
+    }
+}
