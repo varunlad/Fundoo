c012ba58cff15524f1c296a622a2686dff5f94c2
varun <varunlad5@gmail.com>
2021-11-27 15:32:15 +0530

    [Varun]Add:Label Controller is created and Add api for lable for User is completed

---
+++ FundooNotesDemo/Controller/LableController.cs
@@ -0,0 +1,41 @@
+﻿using FundooManager.Interface;
+using FundooModel;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FundooNotesDemo.Controller
+{
+    public class LableController : ControllerBase
+    {
+        private readonly ILableManager manager;
+        public LableController(ILableManager manager)
+        {
+            this.manager = manager;
+        }
+        [HttpPost]
+        [Route("api/lableuser")]//API-Application programming interface
+        public async Task<IActionResult> Lable([FromBody] LableModel lable)
+        {
+            try
+            {
+                string result = await this.manager.Lable(lable);
+
+                if (result.Equals("Lable is added Successfully"))
+                {
+                    return this.Ok(new ResponseModel<string>() { Status = true, Message = result });
+                }
+                else
+                {
+                    return this.BadRequest(new ResponseModel<string>() { Status = false, Message = result });
+                }
+            }
+            catch (Exception ex)
+            {
+                return this.NotFound(new ResponseModel<string>() { Status = false, Message = ex.Message });
+            }
+        }
+    }
+}
