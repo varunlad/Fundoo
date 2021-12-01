ce0285071c4acc494e39bc46a3c6f177b206a1d7
varun <varunlad5@gmail.com>
2021-12-01 18:25:00 +0530

    [Varun]Add:StyleCop is Completed and refactor

--- FundooRepository/Context/UserContext.cs
+++ FundooRepository/Context/UserContext.cs
@@ -1,22 +1,42 @@
-﻿using FundooModel;
-using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Text;
-
-
+﻿//// --------------------------------------------------------------------------------------------------------
+// <copyright file="UserContext.cs" company="Bridgelabz">
+//   Copyright © 2021 Company="BridgeLabz"
+// </copyright>
+// <creator name="Varun Hemant Lad"/>
+// ----------------------------------------------------------------------------------------------------------
 namespace FundooRepository.Context
 {
-        public class UserContext : DbContext //Db Context-connection within the database(it is important to connect to a table in the data base and execute query     
+    using FundooModel;
+    using Microsoft.EntityFrameworkCore;
+
+    /// <summary>
+    /// Db Context-connection within the database(it is important to connect to a table in the data base and execute query
+    /// </summary>
+    public class UserContext : DbContext      
         {
             public UserContext(DbContextOptions<UserContext> options) : base(options)
             {
 
             }
-            public DbSet<RegisterModel> UsersTable { get; set; } //DataSet- it is used to represent table
-            public DbSet<NotesModel> Notes { get; set; }
-            public DbSet<CollaboratorModel> Collaborator { get; set; }
-            public DbSet<LableModel> LabelsTable { get; set; }
 
+        /// <summary>
+        /// With Dbset we can Create new table with name UsersTable
+        /// </summary>
+        public DbSet<RegisterModel> UsersTable { get; set; }
+
+        /// <summary>
+        /// With Dbset we can Create new table with name Notes
+        /// </summary>  
+        public DbSet<NotesModel> Notes { get; set; }
+
+        /// <summary>
+        /// With Dbset we can Create new table with name Collaborator
+        /// </summary>
+        public DbSet<CollaboratorModel> Collaborator { get; set; }
+
+        /// <summary>
+        /// With Dbset we can Create new table with name LabelsTable
+        /// </summary>
+        public DbSet<LableModel> LabelsTable { get; set; }
     }
 }
